3dc3a9af23cb4f7b095d4d694d3e0f746acbbbe7
Faraz Rawala <farazhussain.developer@gmail.com>
2026-03-29 08:02:29 +0500

    complete. working

--- agent.js
+++ agent.js
@@ -2,6 +2,7 @@ import Groq from "groq-sdk";
 import readline from "node:readline/promises";
 
 const expenseDB = [];
+const incomeDB = [];
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
@@ -15,6 +16,11 @@ async function callAgent() {
     {
       role: "system",
       content: `You are Josh, a personal finance assistant. Your task is to assist user with their expenses, balances and financial planning.
+            You have access to following tools:
+            1. getTotalExpense({from, to}): string // Get total expense for a time period.
+            2. addExpense({name, amount}): string // Add new expense to the expense database.
+            3. addIncome({name, amount}): string // Add new income to income database.
+            4. getMoneyBalance(): string // Get remaining money balance from database.
             current datetime: ${new Date().toUTCString()}`,
     },
   ];
@@ -51,10 +57,15 @@ async function callAgent() {
         const functionArgs = tool.function.arguments;
         let result = "";
         if (functionName === "getTotalExpense") {
-          result = await getTotalExpense(JSON.parse(functionArgs));
+          result = await getTotalExpense(JSON.parse(functionArgs || "{}"));
         } else if (functionName === "addExpense") {
-          result = await addExpense(JSON.parse(functionArgs));
+          result = await addExpense(JSON.parse(functionArgs || "{}"));
+        } else if (functionName === "addIncome") {
+          result = addIncome(JSON.parse(functionArgs));
+        } else if (functionName === "getMoneyBalance") {
+          result = getMoneyBalance(JSON.parse(functionArgs));
         }
+
         messages.push({
           role: "tool",
           content: result,
@@ -64,12 +75,14 @@ async function callAgent() {
       }
     }
   }
+  rl.close();
 }
 
 export async function getGroqChatCompletion(messages = []) {
   return groq.chat.completions.create({
     messages: [...messages],
     model: "llama-3.3-70b-versatile",
+    temperature: 0.3,
     tools: [
       {
         type: "function",
@@ -113,6 +126,33 @@ export async function getGroqChatCompletion(messages = []) {
           },
         },
       },
+      {
+        type: "function",
+        function: {
+          name: "addIncome",
+          description: "Add new income entry to income database",
+          parameters: {
+            type: "object",
+            properties: {
+              name: {
+                type: "string",
+                description: "Name of the income. e.g., Got salary",
+              },
+              amount: {
+                type: "string",
+                description: "Amount of the income.",
+              },
+            },
+          },
+        },
+      },
+      {
+        type: "function",
+        function: {
+          name: "getMoneyBalance",
+          description: "Get remaining money balance from database.",
+        },
+      },
     ],
   });
 }
@@ -121,13 +161,28 @@ callAgent();
 
 // Get total expense
 
-async function getTotalExpense(fromDate = "2026-01-01", toDate = "2026-12-31") {
+async function getTotalExpense({
+  fromDate = "2026-01-01",
+  toDate = "2026-12-31",
+} = {}) {
   console.log("Getting total expense from ", fromDate, " to ", toDate + "\n");
   return "Total expense is 1000 PKR";
 }
 
 async function addExpense({ name = "", amount = "" }) {
-  // console.log(`Adding ${amount} to expense db for ${name}`);
+  console.log(`Adding ${amount} to expense db for ${name}`);
   expenseDB.push({ name, amount });
   return "Added to the database.";
+}
+
+async function addIncome({ name, amount }) {
+  incomeDB.push({ name, amount });
+  return "Added to the income database.";
+}
+
+async function getMoneyBalance() {
+  const totalIncome = incomeDB.reduce((acc, item) => acc + item.amount, 0);
+  const totalExpense = expenseDB.reduce((acc, item) => acc + item.amount, 0);
+
+  return `${totalIncome - totalExpense} INR`;
 }
